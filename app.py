19f825339eaf75e23d6fcdb4a1f6dd002eef3fbd
Laurenz Tophof <laurenztophof@gmail.com>
2022-06-04 15:34:05 +0200

    done

--- app.py
+++ app.py
@@ -5,20 +5,42 @@ from PIL import Image
 image = Image.open('../wally_foto.jpeg')
 
 st.title('Where is Wally?')
+
 wally = st.image(image, width=700)
 
+def content():
+
+    option = st.radio(
+     'How would you like to upload a picture of Wally?',
+     ('Upload file', 'Take photo through camera'))
+
+    if option == 'Upload file':
+        uploader = st.file_uploader('Upload Picture with Wally in it here:')
+
+    if option == 'Take photo through camera':
+        uploader = st.camera_input('Take a photo of Wally')
 
 
-raw_picture = st.file_uploader('Upload Picture with Wally in it here:')
-with st.container():
+
+    button = st.button('Start searching for Wally')
+
+    if uploader:
+        suc = st.success('Photo uploaded')
+
+
     with st.spinner('Wait for it...'):
 
-        if raw_picture:
+        if button and not uploader:
+            st.error('You have not uploaded any picture. Upload picture and try again.')
+
+
+        if uploader and button:
             wally.empty()
-            np_picture = np.load(raw_picture)
-
-
-        if raw_picture:
-            st.markdown('Wally is found!')
+            np_picture = np.load(uploader)
+            suc.empty()
+            st.success('Wally is found!')
             st.image(np_picture)
             st.balloons()
+
+
+content()
